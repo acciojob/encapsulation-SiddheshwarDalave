fd0e4f581afe3942d9452946fc48913402ca95ae
Siddheshwar <siddheshwardalave9119@gmail.com>
2022-10-11 09:42:29 +0530

    Encapsulation P01

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String args[]){
+      RWOnly rw=new RWOnly();
+      //System.out.println(rw.name);
+      //ava: name has private access in com.driver.RWOnly
+      rw.setName("Person");
+      String output=rw.getName();
+      System.out.println(output);
+  }
 }
